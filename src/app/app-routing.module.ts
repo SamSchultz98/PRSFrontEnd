e03edcc75c266e11cadf95ea0c47f2b7972abb6d
Sam Schultz <sam.schultz@outlook.com>
2022-11-01 15:22:43 -0400

    FoundationOfFrontEndBeforeBinding

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './misc/home/home.component';
+import { AboutComponent } from './misc/about/about.component';
+import { E404Component } from './misc/e404/e404.component';
+import { MenuComponent } from './misc/menu/menu.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",redirectTo:"/home",pathMatch:'full'},
+
+
+  {path:"**",component:E404Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
